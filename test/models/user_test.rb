08e1ce4eaf005905cec7ee26990a693e0b4e9818
namimatsu <cybermissions@CM-PC-NO-068.localdomain>
2021-09-22 12:25:07 +0900

    Implement advanced login

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -32,23 +32,23 @@ class UserTest < ActiveSupport::TestCase
   end
 
   test "email validation should accept valid addresses" do
-  valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
-  first.last@foo.jp alice+bob@baz.cn]
-  valid_addresses.each do |valid_address|
-    @user.email = valid_address
-    assert @user.valid?, "#{valid_address.inspect} should be valid"
+    valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
+    first.last@foo.jp alice+bob@baz.cn]
+    valid_addresses.each do |valid_address|
+      @user.email = valid_address
+      assert @user.valid?, "#{valid_address.inspect} should be valid"
+    end
   end
-end
 
 
   test "email validation should reject invalid addresses" do
-      invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
-      foo@bar_baz.com foo@bar+baz.com]
-      invalid_addresses.each do |invalid_address|
+        invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
+        foo@bar_baz.com foo@bar+baz.com]
+    invalid_addresses.each do |invalid_address|
         @user.email = invalid_address
         assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
+    end
   end
-end
 
  test "email addresses should be unique" do
         duplicate_user = @user.dup
@@ -67,4 +67,7 @@ end
     assert_not @user.valid?
   end
 
+  test "authenticated? should return false for a user with nil digest" do
+   assert_not @user.authenticated?('')
+  end
 end
