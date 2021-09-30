341c17f7bd6c00bded797d8a2458b22f92d7b69b
k-nmmt <k.namimatsu@cybermissions.co.jp>
2021-09-30 18:23:44 +0900

    13章 浪松

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,9 @@
 class StaticPagesController < ApplicationController
   def home
+    if logged_in? #ログインしているなら
+    @micropost = current_user.microposts.build #現在のユーザーのマイクロポストを作る
+    @feed_items = current_user.feed.paginate(page: params[:page])
+    end
   end
 
   def help
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     redirect_to root_url and return unless @user.activated? #ユーザーが有効化されていなければ表示せずルートへ飛ばす
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
 
   def new
@@ -58,14 +59,6 @@ class UsersController < ApplicationController
   #許可する属性リストにadminは含まれていない
 
   # beforeアクション
-  # ログイン済みユーザーかどうか確認
-  def logged_in_user
-    unless logged_in? #セッションヘルパーのログドインメソッド。unlessはifの反対
-      store_location
-      flash[:danger] = "Please log in."
-      redirect_to login_url
-    end
-  end
 
   #正しいユーザーかどうか確認
   def correct_user
